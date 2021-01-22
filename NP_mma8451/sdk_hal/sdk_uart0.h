f2dcb94c5d3a67a8127a84bd1d60dabaad6ea01a
DESKTOP-6936U4N\hogar <paralogik1996@gmail.com>
2021-01-22 18:07:39 -0500

    Mejoras en los archivos sdk_gpio.c,.h,_i2c0.c,i2c0.h,_uart0.c,_uart0.h

---
+++ NP_mma8451/sdk_hal/sdk_uart0.h
@@ -0,0 +1,63 @@
+/*! @file : sdk_uart0.h
+ * @author Jose .o
+ * @version 1.0.0
+ * @details
+ *
+ */
+#ifndef SDK_UART0_H_
+#define SDK_UART0_H_
+/*******************************************************************************
+ * Includes                                                                    *
+ *******************************************************************************/
+#include "fsl_common.h"
+
+/*!
+ * @addtogroup SDK_HAL
+ * @{
+ */
+/*!
+ * @addtogroup UART0
+ * @{
+ */
+/*******************************************************************************
+ * Public Definitions                                                          *
+ *******************************************************************************/
+
+/*@brief Longitud del Buffer Circular en bytes para recepción de datos por UART0*/
+#define LENGTH_CIRCULAR_BUFFER_B 64 /*B = bytes*********************************/
+
+/*******************************************************************************
+ * External vars                                                               *
+ *******************************************************************************/
+
+/*******************************************************************************
+ * Public vars                                                                 *
+ *******************************************************************************/
+
+/*******************************************************************************
+ * Public Prototypes                                                           *
+ *******************************************************************************/
+
+/*@brief Inicializar el UART0 en Baud Rate indicado
+ * */
+status_t UART0_SetUp(uint32_t baud_rate); /*SetUp ~= Inicializar*/
+
+/*@brief Calculo del número de Datos Nuevos que existen en el Buffer Circular
+ * @return Número de bytes pendientes por ser procesados
+ * */
+uint8_t UART0_NewDataOnBuffer(void);
+
+/*@brief new_byte apuntador de memoria a donde almacenar byte
+ * @return Status de la Ejecución
+ * @code
+ * 		kStatus_Success
+ * 		kStatus_Fail
+ * @encode
+ * */
+status_t UART0_ReadByteCircularBuffer(uint8_t *new_byte);
+
+/** @} */ // end of UART0 group
+
+/** @} */ // end of SDK_HAL group
+
+#endif /* SDK_UART0_H_ */
