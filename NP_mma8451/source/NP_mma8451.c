c4a5f02e75d2a6cad4c769670aac69352b124d75
DESKTOP-6936U4N\hogar <paralogik1996@gmail.com>
2021-01-22 18:13:35 -0500

    Modificacion de codigo fuente para ejecucion de solo acelerometro sin funcionamiento de leds

--- NP_mma8451/source/NP_mma8451.c
+++ NP_mma8451/source/NP_mma8451.c
@@ -1,5 +1,5 @@
 
-#include "stdio.h"
+#include <stdio.h>
 #include "board.h"
 #include "peripherals.h"
 #include "pin_mux.h"
@@ -7,103 +7,141 @@
 #include "MKL02Z4.h"
 #include "fsl_debug_console.h"
 
-#include "sdk_hal_gpio.h"
-#include "sdk_hal_uart0.h"
-#include "sdk_hal_i2c0.h"
-/*******************************************************************************
- * Definitions
- ******************************************************************************/
-#define MMA851_I2C_DEVICE_ADDRESS	0x1D
-
-#define MMA8451_WHO_AM_I_MEMORY_ADDRESS		0x0D
+/*  SDK_HAL Included Files */
+#include "sdk_uart0.h"
+#include "sdk_gpiob.h"
+#include "sdk_i2c0.h"
 
 /*******************************************************************************
- * Private Prototypes
- ******************************************************************************/
+ * Definitions                                                                 *
+ *******************************************************************************/
 
-/*******************************************************************************
- * External vars
- ******************************************************************************/
+/* UART e I2C Clock */
+#define UART_BAUDRATE	115200U
+#define I2C_BAUDRATE	100000U
 
+/* MMA8451, direccion de registro para acceso por I2C. */
+#define I2C_DEVICE_ADDRESS 		0x1D
+/* MMA8451, ID registrado en a direccion de memoria */
+#define DEVICE_ID				0x1A
+/* MMA8451, direccion de resgistro del Device ID. */
+#define WHO_AM_I 				0x0D
 
-/*******************************************************************************
- * Local vars
- ******************************************************************************/
+/* CharacterArray: Condicionales ACCEL (GPIOA e I2C) */
+#define CHAR_LIST	"R" "r" "G" "g" "B" "b" "M" "m" "X" "x" "Y" "y" "Z" "z"
 
+/*
+ * @brief   Application entry point.
+ */
+int main(void) {
 
-/*******************************************************************************
- * Private Source Code
- ******************************************************************************/
+	uint8_t i;
+  	uint8_t size_char_list = sizeof(CHAR_LIST);
 
+	/* Status UART0 e I2C */
+	status_t uart0_status;
+	status_t i2c0_status;
 
-/*******************************************************************************
- * Public Source Code
- ******************************************************************************/
-    int main(void) {
-	status_t status;
-	uint8_t nuevo_byte_uart;
-	uint8_t	nuevo_dato_i2c;
+	/* Nuevos datos en UART0 y ACCE */
+	uint8_t uart0_new_byte;
+	uint8_t i2c0_data_value[2];
 
-  	/* Init board hardware. */
-    BOARD_InitBootPins();
-    BOARD_InitBootClocks();
-    BOARD_InitBootPeripherals();
+	/* Tempora para direccion de memoria ACCE */
+	uint32_t address_outdata[2];
+
+	/* Axis para calculo de ACCE */
+	int16_t x, y, z;
+
+	/* Init board hardware. */
+	BOARD_InitBootPins();
+	BOARD_InitBootClocks();
+	BOARD_InitBootPeripherals();
 #ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
-    /* Init FSL debug console. */
-    BOARD_InitDebugConsole();
+	/* Init FSL debug console. */
+	BOARD_InitDebugConsole();
 #endif
 
-    (void)uart0Inicializar(115200);	//115200bps
-    (void)i2c0MasterInit(100000);	//100kbps
+	/* Inicializar UART0 al Baudrate indicado */
+	(void) UART0_SetUp(UART_BAUDRATE); /*!< 115200 Bps */
 
-    PRINTF("Usar teclado para controlar LEDs\r\n");
-    PRINTF("r-R led ROJO\r\n");
-    PRINTF("v-V led VERDE\r\n");
-    PRINTF("a-A led AZUL\r\n");
-    PRINTF("M buscar acelerometro\r\n");
+	/* Inicializar I2C0 al Baudrate indicado */
+	(void) I2C0_MasterInit(I2C_BAUDRATE); /*!< 100000 Bps */
+
+	PRINTF("    Use el teclado para:\r\n");
+	PRINTF("    Lectura del Acelerometro\r\n");
+	PRINTF("	Para saber si esta disponible presione:\r\n");
+	PRINTF("	(M)  o  (m) \r\n");
+	PRINTF("	Conocer el valor en g de los ejes:\r\n");
+	PRINTF("	X, presione (X)  o  (x) \r\n");
+	PRINTF("	Y, presione (Y)  o  (y) \r\n");
+	PRINTF("	Z, presione (Z)  o  (z) \r\n");
+	PRINTF("\r\n");
+
+	while (1) {
 
 
-    while(1) {
-    	if(uart0CuantosDatosHayEnBuffer()>0){
-    		status=uart0LeerByteDesdeBuffer(&nuevo_byte_uart);
-    		if(status==kStatus_Success){
-    			printf("dato:%c\r\n",nuevo_byte_uart);
-    			switch (nuevo_byte_uart) {
-				case 'a':
-				case 'A':
-					gpioPutToggle(KPTB10);
-					break;
+		if (UART0_NewDataOnBuffer() > 0) {
 
-				case 'v':
-					gpioPutHigh(KPTB7);
-					break;
-				case 'V':
-					gpioPutLow(KPTB7);
-					break;
+			uart0_status = UART0_ReadByteCircularBuffer(&uart0_new_byte);
 
-				case 'r':
-					gpioPutValue(KPTB6,1);
-					break;
-				case 'R':
-					gpioPutValue(KPTB6,0);
-					break;
+			i2c0_status = I2C0_MasterReadStatusByte(I2C_DEVICE_ADDRESS, WHO_AM_I, DEVICE_ID);
 
-				case 'M':
-					i2c0MasterReadByte(&nuevo_dato_i2c, MMA851_I2C_DEVICE_ADDRESS, MMA8451_WHO_AM_I_MEMORY_ADDRESS);
+			I2C_MasterWriteMode(I2C_DEVICE_ADDRESS, 4, 1);
 
-					if(nuevo_dato_i2c==0x1A)
-						printf("MMA8451 encontrado!!\r\n");
-					else
-						printf("MMA8451 error\r\n");
+			if (uart0_status == kStatus_Success) {
 
-					break;
+				PRINTF("\r\nDato ingresado: %c\r\n", uart0_new_byte);
+
+				I2C0_MasterReadValue(&uart0_new_byte, (uint8_t*) CHAR_LIST,
+						(uint8_t*) i2c0_data_value, I2C_DEVICE_ADDRESS, (uint32_t*) address_outdata);
+
+				for (i = 0; i < size_char_list; i++) {
+					if (uart0_new_byte == CHAR_LIST[i]) {
+
+
+				     	     do
+						 GPIO_PinStatus(&uart0_new_byte, (uint8_t*) CHAR_LIST, 0);
+
+						    while(i==0 || i==2 || i==4);
+
+
+							 do
+						 GPIO_PinStatus(&uart0_new_byte, (uint8_t*) CHAR_LIST, 1);
+
+							while (i == 1 || i == 3 || i == 5);
+
+
+
+						if (i == 6 || i == 7) {
+							if (i2c0_status == kStatus_Success) {
+								PRINTF("\r\nMMA8451Q Encontrado!\r\n");
+							} else {
+								PRINTF("\r\nMMA8451Q Error!\r\n");
+						    }
+						}
+						if (i == 8 || i == 9) {
+							x = (((int16_t) (((i2c0_data_value[0] << 8) | i2c0_data_value[1])))) / 2;
+							PRINTF("\r\nPosicion + y - significativa de: X_MSB: 0x%x  X_LSB: 0x%x, Calculo en X = %4d mg\r\n\n",
+									i2c0_data_value[0], i2c0_data_value[1], x);
+						}
+						 if (i == 10 || i == 11) {
+							y = (((int16_t) (((i2c0_data_value[0] << 8) | i2c0_data_value[1])))) / 2;
+							PRINTF("\r\nPosicion + y - significativa de: Y_MSB: 0x%x  Y_LSB: 0x%x, Calculo en Y = %4d mg\r\n\n",
+									i2c0_data_value[0], i2c0_data_value[1], y);
+						}
+
+						if (i == 12 || i == 13) {
+							z = (((int16_t) (((i2c0_data_value[0] << 8) | i2c0_data_value[1])))) / 2;
+							PRINTF("\r\n Posicion + y - significativa de: Z_MSB: 0x%x  Z_LSB: 0x%x, Calculo en Z = %4d mg\r\n\n",
+									i2c0_data_value[0], i2c0_data_value[1], z);
+						}
+
+					}
 				}
-    		}else{
-    			printf("error\r\n");
-    		}
-    	}
-    }
-
-    return 0 ;
+			}else{
+				PRINTF("\r\nData Error: %c\r\n");
+			}
+		}
+	}
+	return 0;
 }
-
